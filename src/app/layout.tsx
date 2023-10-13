812e5db8d49075501c935a240e126ab2c79021fb
Armaan Mishra <armaanmishra48@gmail.com>
2023-10-14 04:54:31 +0530

    added auth and other pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from '@/components/NavBar'
+import Providers from '@/components/Providers'
+import "react-loading-skeleton/dist/skeleton.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='light'>
+      <Providers>
+<body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <NavBar />
+        {children}</body>
+      </Providers>
     </html>
   )
 }
