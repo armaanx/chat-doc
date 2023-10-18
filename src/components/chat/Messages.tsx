2848503fdf6dc630c67a5cf6227c13e0c613a83b
Armaan Mishra <armaanmishra48@gmail.com>
2023-10-18 21:59:55 +0530

    added optimistic update chat feature

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -3,11 +3,16 @@ import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 import { Loader2, MessageSquare } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import  Message  from "./Message";
+import { useContext, useEffect, useRef } from "react";
+import { ChatContext } from "./ChatContext";
+import { useIntersection } from "@mantine/hooks";
 
 interface MessagesProps {
     fileId: string
 }
-const Messages = ({fileId}: MessagesProps) => { 
+const Messages = ({ fileId }: MessagesProps) => { 
+    const { isLoading: isAiThinking} = useContext(ChatContext)
+
     const { data, isLoading, fetchNextPage} = trpc.getFileMessages.useInfiniteQuery({
         fileId,
         limit: INFINITE_QUERY_LIMIT,
@@ -27,17 +32,31 @@ const Messages = ({fileId}: MessagesProps) => {
         )
     }
     const combinedMessages = [
-        ...(true ? [loadingMessage] : []),
+        ...(isAiThinking ? [loadingMessage] : []),
         ...(messages ?? [])
     ]
-    return <div className="flex max-h-[calc(100vh - 3.5rem - 7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+
+    
+    const lastMessageRef = useRef<HTMLDivElement>(null);
+    const { ref, entry } = useIntersection({
+        root: lastMessageRef.current,
+        threshold: 1,
+    })
+
+    useEffect(() => {
+        if (entry?.isIntersecting) {
+            fetchNextPage();
+        }
+    },[entry, fetchNextPage])
+
+    return <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
         {combinedMessages && combinedMessages.length > 0 ? (
             combinedMessages.map((msg, i) => {
                 const isNextMessageSamePerson = combinedMessages[i - 1]?.isUserMessage ===
                     combinedMessages[i]?.isUserMessage;
                 
                 if (i === combinedMessages.length - 1) {
-                return <Message key={msg.id} isNextMessageSamePerson={isNextMessageSamePerson} message={msg} />
+                return <Message ref={ref} key={msg.id} isNextMessageSamePerson={isNextMessageSamePerson} message={msg} />
                 } else {
                     return <Message key={msg.id} isNextMessageSamePerson={isNextMessageSamePerson} message={msg} />
             }
